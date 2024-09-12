0aa8d03cd57f50eaea75598039a5503dc598fac1
setPedro <pedross4789@gmail.com>
2024-09-12 19:22:35 +0200

    feat: pagination

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -8,113 +8,116 @@ import {
     getPaginationRowModel,
     flexRender,
     ColumnDef,
-    PaginationState,
 } from '@tanstack/react-table'
 import { cn } from '@/utils'
 
 type Props<T> = {
     data: T[]
     columns: ColumnDef<T, any>[]
+    pageIdx: number
+    setPageIdx: (x: number) => void
+    count: number
+    isLoading: boolean
     minWidth?: number
 }
 
-export default function Table<T>({ data, columns, minWidth = 800 }: Props<T>) {
-    const [{ pageIndex, pageSize }, setPagination] =
-        React.useState<PaginationState>({
-            pageIndex: 0,
-            pageSize: 15,
-        })
-
+export default function Table<T>({
+    data,
+    columns,
+    pageIdx,
+    setPageIdx,
+    count,
+    isLoading,
+    minWidth = 800,
+}: Props<T>) {
     const table = useReactTable({
         data,
         columns,
-        state: {
-            pagination: { pageIndex, pageSize },
-        },
         getCoreRowModel: getCoreRowModel(),
         getSortedRowModel: getSortedRowModel(),
         getPaginationRowModel: getPaginationRowModel(),
-        onPaginationChange: setPagination,
     })
+
+    const pageCount = Math.ceil(count / 15)
 
     return (
         <div className="flex h-full w-full flex-col justify-between gap-6 overflow-hidden">
-            <div className="overflow-y-scroll overflow-x-auto custom-scrollbar">
-                <table
-                    className="table-auto w-full"
-                    style={{
-                        minWidth: `${minWidth}px`,
-                    }}
-                >
-                    <thead>
-                        {table.getHeaderGroups().map((headerGroup) => (
-                            <tr key={headerGroup.id}>
-                                {headerGroup.headers.map((header) => (
-                                    <th
-                                        key={header.id}
-                                        className="text-left text-sm opacity-50"
-                                    >
-                                        {flexRender(
-                                            header.column.columnDef.header,
-                                            header.getContext()
-                                        )}
-                                    </th>
-                                ))}
-                            </tr>
-                        ))}
-                    </thead>
-                    <tbody>
-                        {table.getRowModel().rows.map((row) => (
-                            <tr key={row.id}>
-                                {row.getVisibleCells().map((cell) => (
-                                    <td key={cell.id} className="py-3">
-                                        {flexRender(
-                                            cell.column.columnDef.cell,
-                                            cell.getContext()
-                                        )}
-                                    </td>
-                                ))}
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
+            <div className="flex-grow overflow-y-scroll overflow-x-auto custom-scrollbar">
+                {isLoading ? (
+                    <div className="w-full h-full flex items-center justify-center border">
+                        <p>Loading Transactions...</p>
+                    </div>
+                ) : (
+                    <table
+                        className="table-auto w-full"
+                        style={{
+                            minWidth: `${minWidth}px`,
+                        }}
+                    >
+                        <thead>
+                            {table.getHeaderGroups().map((headerGroup) => (
+                                <tr key={headerGroup.id}>
+                                    {headerGroup.headers.map((header) => (
+                                        <th
+                                            key={header.id}
+                                            className="text-left text-sm opacity-50"
+                                        >
+                                            {flexRender(
+                                                header.column.columnDef.header,
+                                                header.getContext()
+                                            )}
+                                        </th>
+                                    ))}
+                                </tr>
+                            ))}
+                        </thead>
+                        <tbody>
+                            {table.getRowModel().rows.map((row) => (
+                                <tr key={row.id}>
+                                    {row.getVisibleCells().map((cell) => (
+                                        <td key={cell.id} className="py-3">
+                                            {flexRender(
+                                                cell.column.columnDef.cell,
+                                                cell.getContext()
+                                            )}
+                                        </td>
+                                    ))}
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                )}
             </div>
             <div className="flex justify-center">
                 <div className="flex gap-2 w-fit px-2 py-1 rounded-md bg-secondary">
                     <button
-                        onClick={() => table.setPageIndex(0)}
-                        disabled={!table.getCanPreviousPage()}
-                        className={cn(
-                            !table.getCanPreviousPage() && 'opacity-50'
-                        )}
+                        onClick={() => setPageIdx(0)}
+                        disabled={pageCount === 1}
+                        className={cn(pageIdx === 0 && 'opacity-50')}
                     >
                         {'<<'}
                     </button>
                     <button
-                        onClick={() => table.previousPage()}
-                        disabled={!table.getCanPreviousPage()}
-                        className={cn(
-                            !table.getCanPreviousPage() && 'opacity-50'
-                        )}
+                        onClick={() => setPageIdx(pageIdx - 1)}
+                        disabled={pageIdx === 0}
+                        className={cn(pageIdx === 0 && 'opacity-50')}
                     >
                         {'<'}
                     </button>
                     <span>
-                        {pageIndex + 1} / {table.getPageCount()}
+                        {pageIdx + 1} / {pageCount}
                     </span>
                     <button
-                        onClick={() => table.nextPage()}
-                        disabled={!table.getCanNextPage()}
-                        className={cn(!table.getCanNextPage() && 'opacity-50')}
+                        onClick={() => setPageIdx(pageIdx + 1)}
+                        disabled={pageIdx === pageCount}
+                        className={cn(pageIdx === pageCount && 'opacity-50')}
                     >
                         {'>'}
                     </button>
                     <button
-                        onClick={() =>
-                            table.setPageIndex(table.getPageCount() - 1)
-                        }
-                        disabled={!table.getCanNextPage()}
-                        className={cn(!table.getCanNextPage() && 'opacity-50')}
+                        onClick={() => setPageIdx(pageCount - 1)}
+                        disabled={pageIdx === pageCount}
+                        className={cn(pageIdx === pageCount && 'opacity-50')}
                     >
                         {'>>'}
                     </button>
--- src/sections/app/TransactionHistory/index.tsx
+++ src/sections/app/TransactionHistory/index.tsx
@@ -1,55 +1,79 @@
-import { formatTableTxs } from '@/utils'
+'use client'
+
 import Table from '@/components/Table'
+import { transactionColumns } from './transactionColumns'
+import { FormattedTx } from '@/types'
+import { useEffect, useState } from 'react'
+import { PublicKey } from '@solana/web3.js'
 import {
     getTokenAccounts,
     getTransactions,
     ParsedAta,
     TransactionResponse,
 } from 'solana-mirror'
-import { PublicKey } from '@solana/web3.js'
-import { transactionColumns } from './transactionColumns'
-import { FormattedTx } from '@/types'
+import { formatTableTxs } from '@/utils'
 
 type Props = {
     walletAddress: string
 }
 
-export default async function TransactionHistory({ walletAddress }: Props) {
-    let txs: FormattedTx<string>[] = []
-    let isLoading = false
+export default function TransactionHistory({ walletAddress }: Props) {
+    const publicKey = new PublicKey(walletAddress)
 
-    try {
-        let rawTxs = (await getTransactions(
-            new PublicKey(walletAddress)
-        )) as TransactionResponse<string>
-        let atas = (await getTokenAccounts(
-            new PublicKey(walletAddress)
-        )) as ParsedAta<string, string>[]
-        // TODO: default image
-        txs = formatTableTxs(rawTxs.transactions, atas)
-    } catch (e) {
-        console.log('Error fetching transaction history: ', e)
-    }
+    const [isLoading, setIsLoading] = useState(false)
+    const [txs, setTxs] = useState<FormattedTx<string>[]>([])
+    const [atas, setAtas] = useState<ParsedAta<string, string>[]>([])
+    const [count, setCount] = useState(0)
+    const [pageIdx, setPageIdx] = useState(0)
+
+    useEffect(() => {
+        async function fetchAtas() {
+            const _atas = (await getTokenAccounts(publicKey)) as ParsedAta<
+                string,
+                string
+            >[]
+            setAtas(_atas)
+        }
+
+        fetchAtas()
+    }, [])
+
+    useEffect(() => {
+        async function fetchData() {
+            setIsLoading(true)
+            const rawTxs = (await getTransactions(publicKey, [
+                pageIdx * 15,
+                pageIdx * 15 + 14,
+            ])) as TransactionResponse<string>
+
+            setCount(rawTxs.count)
+
+            const formattedTxs = formatTableTxs(rawTxs.transactions, atas)
+            setTxs(formattedTxs)
+            setIsLoading(false)
+        }
+
+        fetchData()
+    }, [walletAddress, pageIdx])
 
     return (
         <div className="w-full h-full lg:w-1/2 flex flex-col gap-6 font-semibold p-6">
             <div className="flex justify-between">
                 <p className="text-2xl">Transaction History</p>
             </div>
-            {isLoading ? (
-                <div className="w-full h-full flex items-center justify-center">
-                    <p>Loading Transactions...</p>
-                </div>
+            {txs.length ? (
+                <Table
+                    data={txs}
+                    columns={transactionColumns}
+                    pageIdx={pageIdx}
+                    setPageIdx={setPageIdx}
+                    count={count}
+                    isLoading={isLoading}
+                />
             ) : (
-                <>
-                    {txs.length ? (
-                        <Table data={txs} columns={transactionColumns} />
-                    ) : (
-                        <div className="w-full h-full flex items-center justify-center ">
-                            <p>No transactions found for this wallet</p>
-                        </div>
-                    )}
-                </>
+                <div className="w-full h-full flex items-center justify-center">
+                    <p>No transactions found for this wallet</p>
+                </div>
             )}
         </div>
     )
