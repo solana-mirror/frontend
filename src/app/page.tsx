39cdf99fb4fd7fb763f97e92b9096f60b56b3e49
config.json <configjson@proton.me>
2024-09-27 17:39:21 +0300

    fix: build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,11 @@ import NavBar from '@/components/Navbar'
 import { SearchInput } from '@/components/SearchInput'
 
 export default function Home() {
+    const rpc = process.env.RPC as string
+
     return (
         <main className="h-screen flex flex-col items-center">
-            <NavBar hasSearch={false} />
+            <NavBar rpc={rpc} hasSearch={false} />
             <div className="flex px-10 flex-col items-center justify-center w-full h-full">
                 <div className="flex flex-col items-center gap-9">
                     <div className="flex flex-col gap-3 max-w-[560px]">
